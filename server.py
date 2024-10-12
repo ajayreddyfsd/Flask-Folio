0e69757da4b6523405d494c7a7bf050babd2d0c7
Ajay Reddy <ajayreddy.fsd@gmail.com>
2024-10-12 07:30:03 -0400

    working on form submission and storing that data in a text file

--- server.py
+++ server.py
@@ -1,31 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# @app.route('/index.html')
-# def index():
-#     return render_template('index.html')
-
-# @app.route('/work.html')
-# def work():
-#     return render_template('work.html')
-
-# @app.route('/works.html')
-# def works():
-#     return render_template('works.html')
-
-# @app.route('/contact.html')
-# def contact():
-#     return render_template('contact.html')
-
-# @app.route('/about.html')
-# def about():
-#     return render_template('about.html')
 
 @app.route('/<string:page_name>')
 def desired_page(page_name):
     return render_template(page_name)
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        
+        with open('database.txt', mode='a') as f:
+            emailId, emailSubject, emailBody = data
+            f.write(f'{emailId},{emailSubject},{emailBody}')
+
+        return redirect('thankyou.html')
+    else:
+        return 'something wrong!'
+
 
